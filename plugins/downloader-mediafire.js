ac58d821dba3e522015358d47c42e2505393aebe
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-29 04:13:58 +0100

    Create downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -6,10 +6,10 @@ let mdjon = args.join(' ')
 res = await mediafireDl(mdjon)
 result = `「 *MEDIAFIRE DOWNLOAD* 」
 *Data Berhasil Didapatkan!*
-🆔 Nama : ${res[0].nama}
-📊 Ukuran : ${res[0].size}
-💬 Link : ${res[0].link}
-_Tunggu Proses Upload Media_`
+🆔 اسم الملف : ${res[0].nama}
+📊 حجمه : ${res[0].size}
+💬 رابطه : ${res[0].link}
+_المرجو الانتظار قليلا تأكذ ان الملف الذي طلبته لا يتجاوز 100 mb  لان البوت غير قادر على تحميله ملف كبير اي استفسار راسل صاحب البوت_`
 m.reply(result)
 //await sleep(100)
 conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
