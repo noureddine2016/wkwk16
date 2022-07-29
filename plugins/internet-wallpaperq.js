031ca1bdd1632b59183bafbf306f39d53a1a4e3e
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-29 10:23:10 +0200

    Update internet-wallpaperq.js

--- plugins/internet-wallpaperq.js
+++ plugins/internet-wallpaperq.js
@@ -10,11 +10,11 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-  await conn.sendFile(m.chat, img.url_image, 'wallpaper', 'Nih wallpaper!', m)
+  await conn.sendFile(m.chat, img.url_image, 'wallpaper', '^_^;', m)
 }
 handler.help = ['wallpaperq <query>']
 handler.tags = ['internet']
 handler.command = /^wall(paper)?q?$/i
-handler.limit = true
+handler.limit = false
 
 module.exports = handler
