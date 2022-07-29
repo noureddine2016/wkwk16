ca0dd877bdb9b07f260bf650ab3f2de3c5dddcd5
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-29 05:04:26 +0100

    Update sticker-tourl.js

--- plugins/sticker-tourl.js
+++ plugins/sticker-tourl.js
@@ -4,15 +4,15 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada media yang ditemukan'
+  if (!mime) throw 'هذا الامر خاص لرفع صورة او فيديو لموقع *telegra.ph*'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
+${isTele ? '(تم رفع الملف بنجاج وهذا رابطه ♥ )' : '(Tidak diketahui)'}`)
 }
-handler.help = ['tourl <reply image>']
+handler.help = ['tourl  Telegra.ph Upload']
 handler.tags = ['sticker']
 handler.command = /^(upload|tourl)$/i
 
