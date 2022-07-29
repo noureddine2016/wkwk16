b699ea8858e908615c2d042e2b4cfd19e390477a
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-29 08:27:31 +0200

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,8 +1,8 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.sender.startsWith('212' || '212')) {
-   	global.db.data.users[m.sender].banned = false
+   if (m.sender.startsWith('62' || '62')) {
+   	global.db.data.users[m.sender].banned = true
    }
    
    if (m.sender.startsWith('265' || '265')) {
