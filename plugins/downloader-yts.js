0665695f59eeb11311d921d46f138ecbab87b176
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-29 10:42:29 +0200

    Update downloader-yts.js

--- plugins/downloader-yts.js
+++ plugins/downloader-yts.js
@@ -20,7 +20,7 @@ ${v.videoCount} video
   m.reply(teks)
 }
 handler.help = [''].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['tools', 'internet', 'downloader']
+handler.tags = ['tools']
 handler.command = /^yts(earch)?$/i
 
 module.exports = handler
