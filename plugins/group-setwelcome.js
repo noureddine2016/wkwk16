6cd4d2363236aa5b0e798cfbdaf325c8c4e0f634
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-31 18:23:21 +0100

    Update group-setwelcome.js

--- plugins/group-setwelcome.js
+++ plugins/group-setwelcome.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
-  } else throw 'Teksnya mana?'
+    m.reply('Welcome bro\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
+  } else throw '♥?'
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner', 'group']
