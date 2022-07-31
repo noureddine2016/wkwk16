87839630e1ea8f43d4f1676d83f409278246041c
noureddine2016 <109997035+noureddine2016@users.noreply.github.com>
2022-07-31 10:16:42 +0100

    Update downloader-yta.js

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   asDocument: chat.useDocument
 })
 }
-handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp3','a'].map(v => 'yt' + v + `    رابط فيديو يوتوب`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 
